2aaf95b3a7c1961e8bc79801b9ac1fcbbd2c9e1c
Erycles Junio <erycles.junio@dtidigital.com.br>
2022-03-03 14:08:37 -0300

    Ajustando rotas no client

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { VeiculoComponent } from './veiculo/veiculo.component';
 import { ProprietarioComponent } from './proprietario/proprietario.component';
 
 const routes: Routes = [
-  {path:'veiculo',component:VeiculoComponent},
-  {path:'proprietario',component:ProprietarioComponent}
+  { path: '', redirectTo: '/veiculo', pathMatch: 'full' },
+  { path: 'veiculo', component: VeiculoComponent },
+  { path: 'proprietario', component: ProprietarioComponent }
 ];
 
 @NgModule({
